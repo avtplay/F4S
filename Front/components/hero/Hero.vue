333f3dd71406bdcfdd13917f914fd996f2e1fc0c
AVTplay <alex140995@gmail.com>
2019-05-09 11:45:37 +0200

    INIT FRONT

---
+++ Front/components/hero/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title has-text-centered">
+          Des légumes frais chaque jours 
+          <span/>
+        </h1>
+        <h2 class="subtitle has-text-centered">
+          <span class="tag is-success is-large">envie de manger local ? </span>
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background-image: url("../../static/F4S/image_acceuil_1.jpg");
+    }
+    h1, h2 {
+      color: #fff;
+    }
+</style>
